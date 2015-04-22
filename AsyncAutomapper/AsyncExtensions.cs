d54da21dd58fb0028a58a913a7735d687eb0cd08
Jake Ginnivan <jake@ginnivan.net>
2015-04-22 23:25:40 +0100

    Added nuspec

--- AsyncAutomapper/AsyncExtensions.cs
+++ AsyncAutomapper/AsyncExtensions.cs
@@ -107,5 +107,24 @@ namespace Automapper
                 };
             }
         }
+        class InlineAsyncTypeConverter<TSource, TDestination> : AsyncTypeConverter<TSource, TDestination>
+        {
+            private readonly Func<ResolutionContext, TSource, Task<TDestination>> getValue;
+
+            public InlineAsyncTypeConverter(Func<TSource, Task<TDestination>> getValue)
+                : this((context, val) => getValue(val))
+            {
+            }
+
+            public InlineAsyncTypeConverter(Func<ResolutionContext, TSource, Task<TDestination>> getValue)
+            {
+                this.getValue = getValue;
+            }
+
+            protected override Task<TDestination> GetValue(ResolutionContext context, TSource fromValue)
+            {
+                return getValue(context, fromValue);
+            }
+        }
     }
 }
